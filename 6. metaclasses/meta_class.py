a96bc6d71ed9a07e1358803fa5ad6b085ed3e060
RomanovMaxim <romanovmxm@gmail.com>
2019-09-19 13:36:19 +0300

    Created metaclasses

---
+++ 6. metaclasses/meta_class.py
@@ -0,0 +1,26 @@
+class MetaOne(type):
+
+    def __new__(meta, classname, supers, classdict):
+        # Вызывается унаследованным методом type.__call__
+        print('In MetaOne.new:', classname, supers, classdict, sep='\n...')
+        return type.__new__(meta, classname, supers, classdict)
+
+    def __init__(Class, classname, supers, classdict):
+        print('In MetaOne.init:', classname, supers, classdict, sep='\n...')
+        print('...init class object:', list(Class.__dict__.keys()))
+
+
+class Eggs:
+    pass
+
+
+print('making class')
+class Spam(Eggs, metaclass=MetaOne):
+    data = 1
+    def meth(self, arg):
+        pass
+
+
+print('making instance')
+X = Spam()
+print('data:', X.data)
